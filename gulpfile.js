74d4b1b693141d643c317662f0271a8aa14970b2
Bimal <bimalvasan@gmail.com>
2015-06-20 10:46:41 -0500

    Included log printing
    Included log printing

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
-var util = require('gulp-util')
-
+var util = require('gulp-util');
+var gulpprint = require('gulp-print');
+var gulpif = require('gulp-if');
+var args = require('yargs').argv;
 
 gulp.task('vet', function () {
-	log('Analyzing source with JSHint anf JSCS');
+	log('Analyzing source with JSHint and JSCS');
 	return gulp
 		.src([
-			'./src/**/*.js'
-			//'./*.js' //Include files (like gulpfile.js) outside src also
+			'./src/**/*.js',
+			'./*.js'
 		])
+		.pipe(gulpif(args.verbose,	gulpprint()))
 		.pipe(jscs())
 		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish', {
+		.pipe(jshint.reporter('default', {
 			verbose: true
-		}));
+		}))
+		.pipe(jshint.reporter('fail'));
 });
 
 function log(msg) {
