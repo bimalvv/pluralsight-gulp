c262bb2771b7df5750a5c1e65216a889dc9112b1
Bimal <bimalvasan@gmail.com>
2015-06-21 15:36:10 -0500

    Added wire dependency to html
    Added wire dependency to html

--- gulp.config.js
+++ gulp.config.js
@@ -1,13 +1,44 @@
-module.exports = function () {
+module.exports = function() {
 	var client = './src/client/';
+	var clientApp = client + 'app/';
+	var server = './src/server/';
+	var temp = './.tmp/';
+
 	var config = {
-		temp: './.tmp/',
 		// All js files
 		alljs: [
 			'./src/**/*.js',
 			'./*.js'
 		],
-		less: client + 'styles/styles.less'
+		client: client,
+		css: temp + 'styles.css',
+		index: client + 'index.html',
+		js: [
+			clientApp + '**/*.module.js',
+			clientApp + '**/*.js',
+			'!' + clientApp + '**/*.spec.js'
+		],
+		less: client + 'styles/styles.less',
+		server: server,
+		temp: temp,
+		/* Bower and NPM locations */
+		bower: {
+			json: require('./bower.json'),
+			directory: './bower_components/',
+			ignorePath: '../..'
+		},
+		/* Node settings*/
+		defaulPort: 7203,
+		nodeServer: './src/server/app.js'
+	};
+
+	config.getWiredepDefaultOptions = function() {
+		var options = {
+			bowerJson: config.bower.json,
+			directory: config.bower.directory,
+			ignorePath: config.bower.ignore
+		};
+		return options;
 	};
 
 	return config;
--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,9 @@ var $ = require('gulp-load-plugins')({
 	lazy: true
 });
 
-gulp.task('vet', function () {
+var port = process.env.PORT || config.defaulPort;
+
+gulp.task('vet', function() {
 	log('Analyzing source with JSHint and JSCS');
 	return gulp
 		.src(config.alljs)
@@ -20,30 +22,67 @@ gulp.task('vet', function () {
 		.pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('styles', ['clean-styles'], function () {
+gulp.task('styles', ['clean-styles'], function() {
 	log('Compiling Less --> CSS');
 
 	return gulp
 		.src(config.less)
 		.pipe($.plumber())
 		.pipe($.less())
-		//.on('error', errorLogger)
-		.pipe($.autoprefixer({
-			browsers: ['last 2 version', '> 5%']
-		}))
+	//.on('error', errorLogger)
+	.pipe($.autoprefixer({
+		browsers: ['last 2 version', '> 5%']
+	}))
 		.pipe(gulp.dest(config.temp));
 });
 
-gulp.task('clean-styles', function (done) {
+gulp.task('clean-styles', function(done) {
 	var files = config.temp + '**/*.css';
 	clean(files, done);
 });
 
-gulp.task('less-watcher', function () {
+gulp.task('less-watcher', function() {
 	gulp.watch([config.less], ['styles']);
 });
 
-function errorLogger(error){
+gulp.task('wiredep', function() {
+	log('Wire up the bower css, js and out app js into the html.');
+	var options = config.getWiredepDefaultOptions();
+	var wiredep = require('wiredep').stream;
+
+	return gulp
+		.src(config.index)
+		.pipe(wiredep(options))
+		.pipe($.inject(gulp.src(config.js)))
+		.pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject', ['wiredep', 'styles'], function() {
+	log('Wire up the app css into the html and calls wiredep.');
+
+	return gulp
+		.src(config.index)
+		.pipe($.inject(gulp.src(config.css)))
+		.pipe(gulp.dest(config.client));
+});
+
+gulp.task('serve-dev', ['inject'], function() {
+	var isDev = true;
+
+	var nodeOptions = {
+		script: config.nodeServer,
+		delayTime: 1,
+		env: {
+			'PORT': port,
+			'NODE_ENV': isDev ? 'dev' : 'build'
+		},
+		watch: [config.server]
+	};
+
+	return $.nodemon(nodeOptions);
+});
+
+function errorLogger(error) {
 	log('*** Start of Error ***');
 	log(error);
 	log('*** End of Error ***');
@@ -56,7 +95,7 @@ function clean(path, done) {
 }
 
 function log(msg) {
-	if (typeof (msg) === 'object') {
+	if (typeof(msg) === 'object') {
 		for (var item in msg) {
 			if (msg.hasOwnProperty(item)) {
 				$.util.log($.util.colors.blue(msg[item]));
